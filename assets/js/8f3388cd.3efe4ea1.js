3bcb3876988268cf8412f76fb6591935134bd43b
denispionicul <bogdenispionicul@gmail.com>
2023-09-06 14:26:43 +0200

    Built and published by Moonwave

---
+++ assets/js/8f3388cd.3efe4ea1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[727],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Party","label":"\u200bParty"},{"type":"link","href":"/api/PartyModule","label":"\u200bParty\u200bModule"}]')}}]);
